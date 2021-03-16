ff97c9144dab76bd13730c83fdd8b95b9aefe3a8
ayuindraswarii <ayuindraswari5@gmail.com>
2021-03-16 21:02:19 +0700

    commit 2

---
+++ application/views/bio_v.php
@@ -0,0 +1,90 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Biodata</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div id="container">
+	<center>
+	<div class="panel panel-info">
+		<div class="panel-heading">
+			<b>BIODATA</b><br>
+		</div>
+		<div class="panel-body">
+			<img src="assets/img/ayu.jpeg" class="rounded">
+			<br><br>
+			<table cellpadding="10px">
+				<tr bgcolor="#F5F5F5">
+					<td colspan="3" align="center"><b>Data Diri</b></td>
+				</tr>
+				<tr>
+					<td>Nama</td>
+					<td>:</td>
+					<td>Ayu Indraswari</td>
+				</tr>
+				<tr>
+					<td>Jenis Kelamin</td>
+					<td>:</td>
+					<td>Perempuan</td>
+				</tr>
+				<tr>
+					<td>Tempat Lahir</td>
+					<td>:</td>
+					<td>Malang</td>
+				</tr>
+				<tr>
+					<td>Tanggal Lahir</td>
+					<td>:</td>
+					<td>13 November 2000</td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td>:</td>
+					<td>ayuindraswari5@gmail.com</td>
+				</tr>
+				<tr bgcolor="#F5F5F5">
+					<td colspan="3" align="center"><b>Pendidikan</b></td>
+				</tr>
+				<tr>
+					<td>TK</td>
+					<td>:</td>
+					<td>TK Permata Bunda</td>
+				</tr>
+				<tr>
+					<td>SD</td>
+					<td>:</td>
+					<td>SDN Sawojajar 5</td>
+				</tr>
+				<tr>
+					<td>SMP</td>
+					<td>:</td>
+					<td>SMPN 20 Malang</td>
+				</tr>
+				<tr>
+					<td>SMA/SMK</td>
+					<td>:</td>
+					<td>SMK Telkom Malang</td>
+				</tr>
+				<tr>
+					<td>Sekarang</td>
+					<td>:</td>
+					<td>Universitas Gajayana Malang</td>
+				</tr>
+			</table>
+		</div>
+		<div class="panel-footer">
+			<b>@2021</b>
+		</div>
+	</center>
+	</div>
+</div>
+
+</body>
+</html>
